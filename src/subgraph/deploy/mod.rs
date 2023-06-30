aac6070367b4130b42bc8e405e1383130a020adc
vishal <vishalkale151071@gmail.com>
2023-06-30 20:55:42 +0000

    added deploy script

--- src/subgraph/deploy/mod.rs
+++ src/subgraph/deploy/mod.rs
@@ -1,6 +1,7 @@
 use anyhow::anyhow;
 use clap::Parser;
-use std::{path::PathBuf, process::Command};
+use mustache::MapBuilder;
+use std::{fs, path::PathBuf, process::Command, str::FromStr};
 
 #[derive(Parser)]
 pub struct Config {
@@ -33,27 +34,167 @@ pub struct Config {
     #[arg(
         short = 'e',
         long = "end-point",
-        default_value = "--node https://api.thegraph.com/deploy/"
+        default_value = "http://localhost:8020/"
     )]
     end_point: Option<String>,
+    // output path
+    #[arg(short = 'o', long = "output-path", default_value = "subgraph.yaml")]
+    output_path: Option<PathBuf>,
+    // Root dir
+    #[arg(short = 'r', long = "root", default_value = "/")]
+    root_dir: Option<PathBuf>,
 }
 
 pub async fn deploy(config: Config) -> anyhow::Result<()> {
-    let graph_access_token = match config.graph_access_token {
-        Some(token) => token,
-        None => return Err(anyhow!("Graph Access Token is not proiveded.")),
+    let network = match config.network {
+        Some(network) => network,
+        None => return Err(anyhow!("No network provided")),
     };
 
-    if config.network.unwrap() != "localhost" {
-        let output = Command::new(format!(
-            "npx graph auth --product hosted-service {}",
-            graph_access_token
-        ))
-        .output()?;
-        if !output.status.success() {
+    let contract = match config.contract_address {
+        Some(address) => address,
+        None => return Err(anyhow!("No contract address provided")),
+    };
+
+    let block_number = match config.block_number {
+        Some(block) => block,
+        None => return Err(anyhow!("No block-number provided")),
+    };
+
+    let output_path = match config.output_path {
+        Some(path) => path,
+        None => return Err(anyhow!("No output path provided")),
+    };
+
+    let subgraph_template = match config.subgraph_template {
+        Some(path) => path,
+        None => return Err(anyhow!("No subgraph-template path provided")),
+    };
+
+    let root_dir = match config.root_dir {
+        Some(path) => path,
+        None => return Err(anyhow!("No root path provided")),
+    };
+
+    let end_point = match config.end_point {
+        Some(val) => val,
+        None => return Err(anyhow!("No end-point provided")),
+    };
+
+    let subgraph_name = match config.subgraph_name {
+        Some(name) => name,
+        None => return Err(anyhow!("No subgraph-name provided provided")),
+    };
+
+    let version_lable = match config.version_lable {
+        Some(label) => label,
+        None => return Err(anyhow!("No version-lable provided provided")),
+    };
+
+    if network.clone().ne(&String::from_str("localhost").unwrap()) {
+        let graph_access_token = match config.graph_access_token {
+            Some(token) => token,
+            None => return Err(anyhow!("Graph Access Token is not proiveded.")),
+        };
+
+        let output = Command::new("bash")
+            .args(&[
+                "-c",
+                format!(
+                    "npx graph auth --product hosted-service {}",
+                    graph_access_token
+                )
+                .as_ref(),
+            ])
+            .output()
+            .expect("Failed graph auth command");
+
+        if output.status.success() {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            println!("{}", stdout);
+        } else {
             let stderr = String::from_utf8_lossy(&output.stderr);
-            println!("command error : {}", stderr)
+            return Err(anyhow!("{}", stderr));
         }
     }
+
+    let data = MapBuilder::new()
+        .insert_str("network", &network)
+        .insert_str("MetaBoard", contract)
+        .insert_str("MetaBoardBlock", block_number)
+        .build();
+
+    let template = fs::read_to_string(subgraph_template.clone()).expect(&format!(
+        "Fail to read {}",
+        subgraph_template.to_str().unwrap()
+    ));
+
+    let renderd = mustache::compile_str(&template)
+        .expect("Failed to compile template")
+        .render_data_to_string(&data)
+        .expect("Failed to render template");
+
+    let _write = fs::write(output_path, renderd)?;
+
+    let output = Command::new("bash")
+        .current_dir(&root_dir)
+        .args(&["-c", "npx graph codegen && npx graph build"])
+        .output()?;
+
+    if output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        println!("{}", stdout);
+    } else {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        return Err(anyhow!("{}", stderr));
+    }
+
+    if network.ne(&String::from_str("localhost").unwrap()) {
+        let output = Command::new("bash")
+            .current_dir(&root_dir)
+            .args(&[
+                "-c",
+                &format!("npx graph deploy {} {}", end_point, subgraph_name),
+            ])
+            .output()
+            .expect("Failed graph deploy command");
+
+        if output.status.success() {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            println!("{}", stdout);
+        } else {
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            return Err(anyhow!("{}", stderr));
+        }
+    } else {
+        let _output = Command::new("bash")
+            .args(&[
+                "-c",
+                &format!("graph create --node {} {}", end_point, subgraph_name),
+            ])
+            .output()
+            .expect("Failed local deploy command");
+
+        let output = Command::new("bash")
+            .current_dir(&root_dir)
+            .args(&[
+                "-c",
+                &format!(
+                    "graph deploy --node {} --ipfs http://localhost:5001 {}  --version-label {}",
+                    end_point, subgraph_name, version_lable
+                ),
+            ])
+            .output()
+            .expect("Failed local deploy command");
+
+        if output.status.success() {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            println!("{}", stdout);
+        } else {
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            return Err(anyhow!("{}", stderr));
+        }
+    }
+
     Ok(())
 }
